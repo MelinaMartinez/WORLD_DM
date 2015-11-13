fd256d2f98ca05976602f6a4df7a38f258004921
bourrelidylan <bourreli.dylan@hotmail.com>
2015-11-13 15:29:06 +0100

    ajout pays

--- function.php
+++ function.php
@@ -1,5 +1,8 @@
 <?php
-
-
-
+function getcountry($pdo) {
+	$sql= 'SELECT DISTINCT Name FROM country;';
+			$country = $pdo->query($sql); 
+			$rowcountry = $country->fetchAll(PDO::FETCH_ASSOC);
+			return $rowcountry;
+}
 ?>
