2dd3de04bcdf54524217d2aab57ec759d3fe5399
MelinaMartinez <melina.martinezg@gmail.com>
2015-11-13 13:28:42 +0100

    Debut

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+
+	<header>
+		<h1>Statistiques mondiales</h1>
+	</header>
+	
+	<article>
+		<!-- partie Monde-->
+	</article>
+
+	<article>
+		<!-- partie Continent-->
+		<select name="" multiple>
+			<option value=""></option>
+		</select>
+
+
+		<h2></h2>
+
+		<p></p>
+
+
+		<h2></h2>
+
+		<p></p>
+	</article>
+
+	<article>
+		<!-- partie PAYS-->
+		<select name="" multiple>
+			<option value=""></option>
+		</select>
+
+
+		<h2></h2>
+
+		<p></p>
+
+
+		<h2></h2>
+
+		<p></p>
+	</article>
+
+
+</body>
+</html>
